b071c5eb29abe35f034b34e5bf87d1aa6c0e3fe8
Matt Martinez <mattm1823@gmail.com>
2017-05-20 16:49:15 +0000

    Joey, Guillermo, Matt, and Ike's project

--- social_network/accounts.py
+++ social_network/accounts.py
@@ -1,13 +1,39 @@
 
 class User(object):
     def __init__(self, first_name, last_name, email):
-        pass
-
+        self.first_name = first_name
+        self.last_name = last_name
+        self.email = email
+        self.following = []
+        self.posts = []
+        
     def add_post(self, post):
-        pass
+        #Update post post.user attribute to self.
+        post.set_user(self)
+        self.posts.append(post)
 
     def get_timeline(self):
-        pass
-
+        list_of_posts = []
+        #get all posts from followers, add to this list, then sort the list.
+        for followed_user in self.following:
+            for post in followed_user.posts:
+                list_of_posts.append(post)
+        return sorted(list_of_posts, key=lambda x: x.timestamp, reverse=True)
+        
     def follow(self, other):
-        pass
+        self.following.append(other)
+
+
+#list = ['a: apple', 2, 1]
+#list.sort() !changes list to [1, 2, 3]
+
+#sorted(list_to_be_operated_on, key, reverse=True/False)
+#sorted(list_of_posts, !Pseudocode: take an object and return timestamp, reverse )
+#Google: sorted list by object attribute
+#lambda - an anonyomous function
+#
+#
+#def gets_timestamp_from_object(an_object):
+#   return an_object.timestamp
+# find_object_timestamp = lambda x : x.timestamp
+# for i in list_of_posts
--- social_network/posts.py
+++ social_network/posts.py
@@ -1,33 +1,49 @@
 from datetime import datetime
 
 
-# class Post(object):
-#     def __init__(self, text, timestamp=None):
-#         pass
-#
-#     def set_user(self, user):
-#         pass
+class Post(object):
+   def __init__(self, text, timestamp=None):
+      pass
+    
+   def set_user(self, user = None):
+      self.user = user
 
 
-# class TextPost(...):  # Inherit properly
-#     def __init__(self, text, timestamp=None):
-#         pass
-#
-#     def __str__(self):
-#         pass
+class TextPost(Post):  # Inherit properly
+    def __init__(self, text, timestamp=None):
+       self.text = text
+       self.timestamp = timestamp or datetime.now()
+       self.user = None
+    
+    def __str__(self):
+       return '@{} {}: "{}"\n\t{}'.format(self.user.first_name,\
+       self.user.last_name, self.text, self.timestamp.strftime('%A, %b %d, %Y'))
 
 
-# class PicturePost(...):  # Inherit properly
-#     def __init__(self, text, image_url, timestamp=None):
-#         pass
-#
-#     def __str__(self):
-#         pass
+class PicturePost(Post):  # Inherit properly
+    def __init__(self, text, image_url, timestamp=None):
+        self.text = text
+        self.image_url = image_url
+        self.timestamp = timestamp or datetime.now()
+        self.user = None
+
+    def __str__(self):
+        return '@{} {}: "{}"\n\t{}\n\t{}'.format(self.user.first_name,\
+        self.user.last_name, self.text, self.image_url, self.timestamp.strftime('%A, %b %d, %Y'))
+#'@Kevin Watson: "Sample post text"\n\thttp://fake-domain.com/images/sample.jpg\n\tTuesday, Jan 10, 2017'
+
+class CheckInPost(Post):  # Inherit properly
+    def __init__(self, text, latitude, longitude, timestamp=None):
+        self.text = text
+        self.latitude = latitude
+        self.longitude = longitude
+        self.timestamp = timestamp or datetime.now()
+        self.user = None
+
+    def __str__(self):
+        return '@{} Checked In: "{}"\n\t{}, {}\n\t{}'.format(self.user.first_name,\
+        self.text, self.latitude, self.longitude, self.timestamp.strftime('%A, %b %d, %Y'))
 
 
-# class CheckInPost(...):  # Inherit properly
-#     def __init__(self, text, latitude, longitude, timestamp=None):
-#         pass
-#
-#     def __str__(self):
-#         pass
+
+#datetime.now().strftime("%A, %B")
